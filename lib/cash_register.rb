1cd7157751c049c351ecbbc51d5e2e7f267ff266
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-04-19 00:26:33 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,15 @@
+class CashRegister
+  attr_accessor :total, :discount, :items, :last_transaction
 
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
+  def add_item(item, cost, quantity=1)
+    self.total += cost * quantity
+    quantity.times { items << item }
+  end
+
+end
